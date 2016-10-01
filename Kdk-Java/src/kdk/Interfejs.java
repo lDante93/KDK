bfd04d23288e3c3e42c6e18caee34341ecca06f5
lDante93 <lDante93>
2016-10-01 17:00:54 +0200

    asdasd

--- Kdk-Java/src/kdk/Interfejs.java
+++ Kdk-Java/src/kdk/Interfejs.java
@@ -12,7 +12,7 @@ public class Interfejs {
 
     public static void main(String[] args) {
         Kot kot = new Kot();
-
+asdasd
         System.out.print("Podaj imię kota: ");
         kot.setImie(getUserInput());
 
